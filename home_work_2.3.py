8eb7a58d4ace2b562be57284c49d3514b4a9f549
Sayberix <mr.maaks@gmail.com>
2022-12-03 10:22:21 +0700

    задача выполнена

--- home_work_2.3.py
+++ home_work_2.3.py
@@ -3,3 +3,13 @@
 
 # Пример:
 # [2, 3, 5, 9, 3] -> на нечётных идексах элементы 3 и 9, ответ: 12
+
+def sumOddElements(f_mylist: list):
+    sum = 0
+    for i in range(len(f_mylist)):
+        if i % 2 == 1:
+            sum += f_mylist[i]
+    return sum
+
+mylist = [2, 3, 5, 9, 3]
+print('сумма элементов с нечетными индексами:',sumOddElements(mylist))
