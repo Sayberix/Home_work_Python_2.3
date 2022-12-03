09ea05cb3e15f10229d358a40df5b89ea1bb1cf3
Sayberix <mr.maaks@gmail.com>
2022-12-03 12:36:01 +0700

    задача выполнена

--- home_work_2.3.py
+++ home_work_2.3.py
@@ -4,3 +4,21 @@
 # Пример:
 # - [2, 3, 4, 5, 6] => [12, 15, 16];
 # - [2, 3, 5, 6] => [12, 15]
+
+# функция правильного математического округления до целого числа
+def int_round(num: float) -> int:
+    if int(num * 10) % 10 > 4:
+        return int(num + 1)
+    else:
+        return int(num)
+
+def multiplication_pairs(f_my_list: list)  -> list[int]:
+    resault = []
+    range_list = int_round(len(f_my_list)/2)
+    #range_list = int_round(2.4)
+    for i in range(int(range_list)):
+        resault.append(f_my_list[i] * f_my_list[int(range_list) + 1 - i])
+    return resault
+
+my_list = [2, 3, 5, 6]
+print('результат:',multiplication_pairs(my_list))
