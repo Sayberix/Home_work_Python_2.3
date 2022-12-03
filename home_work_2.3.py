9b5ca64ed227cc78b6f41add6a18b872027ed56f
Sayberix <mr.maaks@gmail.com>
2022-12-03 16:44:35 +0700

    задача выполнена

--- home_work_2.3.py
+++ home_work_2.3.py
@@ -1,21 +1,20 @@
-# 3. Задайте список из вещественных чисел. Напишите программу, 
-# которая найдёт разницу между максимальным и минимальным значением дробной части элементов.
+# 4. Напишите программу, которая будет преобразовывать десятичное число в двоичное.
 
 # Пример:
-# - [1.1, 1.2, 3.1, 10.01] => 0.19
+# 45 -> 101101
+# 3 -> 11
+# 2 -> 10
 
-import math as m
+def conversion_binary(f_number: int) -> str:
+    resault = ''
+    while f_number != 0:
+        if f_number % 2 == 0:
+            f_number /= 2
+            resault += '0'
+        else:
+            f_number = (f_number - 1) / 2
+            resault += '1'
+    return resault
 
-def find_diff(f_mylist: list) -> int:
-    min, max = 1, 0
-    for i in range(len(f_mylist)):
-        number, _ = m.modf(f_mylist[i])
-        number = round(number,len(str(f_mylist[i]).split('.')[1]))
-        if number > max:
-            max = number
-        if number < min:
-            min = number
-    return max - min
-
-my_list = [1.1, 1.2, 3.1, 10.01]
-print(find_diff(my_list))
+number = int(input('введите число в десятичной системе: '))
+print('число -',number,'в двоичной системе равно:',conversion_binary(number))
